b910807956dd0ab640d14c4c8a34ba0337a1d356
Yujin Sung <rnlduadns98@naver.com>
2021-01-12 23:42:20 +0900

    Python 설치하기

---
+++ python_intro.py
@@ -0,0 +1,38 @@
+if 3>2:
+    print('it works!')
+if 5>2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
+
+
+volume = 57 #volume 값을 바꿔보세요 
+if volume <20:
+    print("It's kinda quiet.")
+elif 20 <= volume <40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume <80:
+    print("Nice for parties")
+elif 80 <= volume <100:
+    print("A bit loud!")
+else:
+    print("My ears are hurting! : (")
+
+def hi(name):
+   print('Hi'+name + '!')
+        
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girls')
